6dace933b362f33b92304ff4994c63b884a79070
Krasva687 <krasva687@gmail.com>
2021-08-28 17:50:52 +0300

    Добавлено решение problemС

--- src/main/kotlin/ProblemC.kt
+++ src/main/kotlin/ProblemC.kt
@@ -3,7 +3,19 @@ class ProblemC {
     fun main() {
 
         //ваша реализация в методе main
+        //Считываем количество студентов и их скилы
+        val countOfStudent = readInt()
+        val studentAndSkills = readIntArray(countOfStudent)
+        //сортируем муровень скилов по
+        studentAndSkills.sort()
 
+        var countExcersice = 0
+
+        for(i in 0..(countOfStudent - 1) step 2){
+            countExcersice += studentAndSkills[i+1] - studentAndSkills[i]
+
+        }
+        println(countExcersice)
     }
 
 }
--- src/main/kotlin/Reader.kt
+++ src/main/kotlin/Reader.kt
@@ -7,5 +7,6 @@ fun readStrings() = readln().trim().split(" ")
 
 fun readIntArray(size:Int) = readStrings().run { IntArray(size) { get(it).toInt() } }
 
+
 fun readListInt() = readStrings().map { it.toInt() }
 fun readListLong() = readStrings().map { it.toLong() }
