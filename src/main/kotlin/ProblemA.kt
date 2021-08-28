ce59b9a2040afd790f25e56532010cfe59748cd8
Krasva687 <krasva687@gmail.com>
2021-08-28 16:20:51 +0300

    Добавлено решение problemA

--- src/main/kotlin/ProblemA.kt
+++ src/main/kotlin/ProblemA.kt
@@ -3,6 +3,30 @@ class ProblemA {
     fun main() {
 
         //ваша реализация в методе main
+        //читаем число запросов из первой строки
+        val frogCount = readInt()
+
+        //начинаем цикл для прохода по всем запросам
+        for(i in 1..frogCount){
+        //считываем массив прыжков лягушки
+        val frogJumps = readListInt()
+        //обнуляем первую координату для лягушки
+            var frogEndCoordinate = 0
+            //считаем конечную координату для каждого запроса
+        for(j in frogJumps.indices){
+
+            if((j + 1) % 2 == 1){
+                frogEndCoordinate += frogJumps[j]
+
+            } else if((j + 1) % 2 == 0) {
+
+                frogEndCoordinate -= frogJumps[j]
+            }
+
+        }
+            //выводим в консоль где остановилась лягушка
+            println(frogEndCoordinate)
+        }
 
     }
 
